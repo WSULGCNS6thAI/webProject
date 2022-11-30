6b01acd5a176c4f1d3e694eb0abf4b774e8c3249
519-bin <115973280+519-bin@users.noreply.github.com>
2022-11-30 15:24:14 +0900

    2-12까지

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -5,16 +5,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.RequiredArgsConstructor;
 import java.util.List;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
     private final QuestionRepository questionRepository;
 
-    @RequestMapping("/question/list")
+    @RequestMapping("/list")
     public String list(Model model)  {
         List<Question> questionList = this.questionRepository.findAll();
         model.addAttribute("quesionList", questionList);
         return "question_list";
+    }
+
+    @RequestMapping(value = "/detail/{id}")
+    public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
